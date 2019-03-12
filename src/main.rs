b706a5a28bca2c600124f07125c30816a7f63691
yuta24 <yuta24@bivre.com>
2019-03-12 19:29:45 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+use http::{Request, Response};
+
 fn main() {
-    println!("Hello, world!");
+    let base_url = "https://api.figma.com";
+    let access_token_key = "FIGMA_ACCESS_TOKEN";
+
+    let access_token = match env::var(access_token_key) {
+        Ok(val) => val,
+        Err(err) => {
+            println!("{}: {}", err, access_token_key);
+            process::exit(1);
+        },
+    };
+
+    println!("{}", access_token)
 }
