9771bc62a38865f2e190e110cae21ea0d842db23
yuta24 <yuta24@bivre.com>
2019-03-26 23:12:45 +0900

    Improve

---
+++ src/exporter/mod.rs
@@ -0,0 +1,86 @@
+use std::result::Result;
+use std::collections::{HashSet, HashMap};
+use std::str::FromStr;
+
+use log::debug;
+use failure::Error;
+
+use super::figma;
+use super::figma::Node;
+use super::design::Source;
+
+pub enum StyleType {
+    TEXT,
+    COLOR,
+}
+
+impl FromStr for StyleType {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "text" => Ok(StyleType::TEXT),
+            "color" => Ok(StyleType::COLOR),
+            _ => Err("Error"),
+        }
+    }
+}
+
+pub struct Exporter {
+    access_token: String,
+    team_id: String,
+    style_type: StyleType,
+}
+
+impl Exporter {
+    pub fn new(access_token: String, team_id: String, style_type: String) -> Exporter {
+        return Exporter { access_token: access_token, team_id: team_id, style_type: StyleType::from_str(&style_type).unwrap() };
+    }
+
+    pub fn execute(&self) -> Result<(), Error> {
+        let client = figma::Client::new(self.access_token.clone());
+
+        let styles = client.get_styles(&self.team_id)?;
+
+        let file_keys = styles.iter().map(|style| {
+            return style.file_key.clone();
+        });
+
+        let file_keys: HashSet<String> = file_keys.into_iter().collect();
+        let mut file_nodes = HashMap::new();
+        for file_key in file_keys {
+        let file_styles: Vec<String> = styles.clone().into_iter().filter(|style| style.file_key == *file_key).map(|style| style.node_id).collect();
+            file_nodes.insert(file_key, file_styles);
+        }
+
+        debug!("{:#?}", file_nodes);
+
+        let mut responses = Vec::new();
+        for (file_key, file_node_ids) in file_nodes.iter() {
+            let nodes = client.get_file_nodes(&file_key, &file_node_ids)?;
+            responses.push(nodes);
+        }
+
+        let nodes: Vec<Node> = responses.iter()
+            .flat_map(|response| response)
+            .cloned()
+            .collect();
+
+        let mut source = Source { rects: Vec::new(), texts: Vec::new() };
+        for node in nodes {
+            match node {
+                Node::Rectangle { r } => {
+                    source.rects.push(r);
+                },
+                Node::Text { t } => {
+                    source.texts.push(t);
+                },
+            }
+        }
+
+        debug!("{:#?}", source);
+
+        source.generate();
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,19 @@ extern crate clap;
 
 mod figma;
 mod design;
+mod exporter;
 
-use std::collections::{HashSet, HashMap};
 use std::{env, process};
 
 use failure::Error;
 
 use env_logger;
-use log::{info, debug};
 use clap::{App, Arg, SubCommand};
-
-use figma::Node;
-use design::Source;
 
 const ACCESS_TOKEN_KEY: &str = "FIGMA_ACCESS_TOKEN";
 
 fn main() -> Result<(), Error> {
     env_logger::init();
-
-    let access_token = match env::var(ACCESS_TOKEN_KEY) {
-        Ok(val) => val,
-        Err(err) => {
-            println!("{}: {}", err, ACCESS_TOKEN_KEY);
-            process::exit(1);
-        },
-    };
 
     let matches = App::new("exporter")
         .version("0.0.1")
@@ -36,15 +24,15 @@ fn main() -> Result<(), Error> {
         .about("Figma style exporter")
         .arg(Arg::with_name("team-id")
             .help("Set figma's team id")
-            .short("t")
             .long("team-id")
+            .required(true)
             .takes_value(true)
         )
-        .subcommand(SubCommand::with_name("color")
-            .about("generate color style")
-        )
-        .subcommand(SubCommand::with_name("text")
-            .about("generate text style")
+        .arg(Arg::with_name("style")
+            .help("style type")
+            .long("style-type")
+            .required(true)
+            .takes_value(true)
         )
         .get_matches();
 
@@ -55,50 +43,22 @@ fn main() -> Result<(), Error> {
             process::exit(1);
         },
     };
+    let style_type = match matches.value_of("style") {
+        Some(style_type) => style_type,
+        None => {
+            println!("Set style type");
+            process::exit(1);
+        },
+    };
 
-    let client = figma::Client::new(access_token);
+    let access_token = match env::var(ACCESS_TOKEN_KEY) {
+        Ok(val) => val,
+        Err(err) => {
+            println!("{}: {}", err, ACCESS_TOKEN_KEY);
+            process::exit(1);
+        },
+    };
 
-    let styles = r#try!(client.get_styles(&team_id));
-
-    let file_keys = styles.iter().map(|style| {
-        return style.file_key.clone();
-    });
-
-    let file_keys: HashSet<String> = file_keys.into_iter().collect();
-    let mut file_nodes = HashMap::new();
-    for file_key in file_keys {
-    let file_styles: Vec<String> = styles.clone().into_iter().filter(|style| style.file_key == *file_key).map(|style| style.node_id).collect();
-        file_nodes.insert(file_key, file_styles);
-    }
-
-    debug!("{:#?}", file_nodes);
-
-    let mut responses = Vec::new();
-    for (file_key, file_node_ids) in file_nodes.iter() {
-        let nodes = r#try!(client.get_file_nodes(&file_key, &file_node_ids));
-        responses.push(nodes);
-    }
-
-    let nodes: Vec<Node> = responses.iter()
-        .flat_map(|response| response)
-        .cloned()
-        .collect();
-
-    let mut source = Source { rects: Vec::new(), texts: Vec::new() };
-    for node in nodes {
-        match node {
-            Node::Rectangle { r } => {
-                source.rects.push(r);
-            },
-            Node::Text { t } => {
-                source.texts.push(t);
-            },
-        }
-    }
-
-    debug!("{:#?}", source);
-
-    source.generate();
-
-    Ok(())
+    let exporter = exporter::Exporter::new(access_token, team_id.to_string(), style_type.to_string());
+    exporter.execute()
 }
