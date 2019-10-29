bac570985a6f649047aac24f6a144a2e6edc83db
Hinomori Hiroya <nexearth@gmail.com>
2019-10-29 17:12:00 +0900

    [Change] Font weight 700 or HiraginoSans-W6 or HiraKakuPro-W6 = .bold

--- templates/swift/TextStyle.swift
+++ templates/swift/TextStyle.swift
@@ -1,7 +1,11 @@
 import Foundation;
 
 public enum TextStyle {
-    {% for text in texts %}
-        static let {{text.name|slugify|replace(from="-", to="_")|camelcase}} = UIFont(name: "{{text.style.fontPostScriptName}}", size: {{text.style.fontSize}}) ?? UIFont.systemFont(ofSize: {{text.style.fontSize}}, weight: .regular)
-    {% endfor %}
+{% for text in texts %}
+    {% if text.style.fontPostScriptName == 'HiraginoSans-W6' or text.style.fontPostScriptName == 'HiraKakuPro-W6' or text.style.fontWeight == 700 %}
+    static let {{text.name|slugify|replace(from="-", to="_")|camelcase}} = UIFont(name: "{{text.style.fontPostScriptName}}", size: {{text.style.fontSize}}) ?? UIFont.systemFont(ofSize: {{text.style.fontSize}}, weight: .bold)
+    {% else %}
+    static let {{text.name|slugify|replace(from="-", to="_")|camelcase}} = UIFont(name: "{{text.style.fontPostScriptName}}", size: {{text.style.fontSize}}) ?? UIFont.systemFont(ofSize: {{text.style.fontSize}}, weight: .regular)
+    {% endif %}
+{% endfor %}
 }
